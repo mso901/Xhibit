0cd877f183b58c3abd74013b15a95e33260e539c
[AI10] 임도헌 <ldh2233@gmail.com>
2024-05-08 23:11:57 +0900

    Feat: otherspage usercard complete

--- client/views/js/otherspage.js
+++ client/views/js/otherspage.js
@@ -1,4 +1,40 @@
-const portfolio = document.querySelector(".resume-content");
+// 포폴값 받아오는 함수
+async function getUserPortfolio() {
+  // const userId =
+  let query = window.location.search;
+  let idParams = new URLSearchParams(query);
+  let userId = idParams.get("userId");
+
+  const BASE_URL = "http://localhost:3000";
+
+  const baseInstance = axios.create({
+    baseURL: BASE_URL, // 기본 URL 설정
+  });
+  const response = await baseInstance.get(`/api/${userId}`);
+  const { user, education, award, certificate, project } = response.data;
+  console.log(user);
+
+  const myCardDiv = document.querySelector(".my-card");
+  const { name, email, introduce } = user[0];
+
+  myCardDiv.innerHTML = `<div class="my-card-header">
+  <img
+    src="../../public/images/img-profile01.png"
+    alt="profile_img"
+    class="profile_img"
+  />
+  <div class="my-card-intro">
+    <p class="card-name">${name}</p>
+    <p class="card-email">${email}</p>
+  </div>
+</div>
+
+<div class="my-card-content">
+  ${introduce}
+</div>`;
+}
+
+getUserPortfolio();
 
 const portfolioSection = [
   { className: "education", title: "학력" },
@@ -7,26 +43,23 @@ const portfolioSection = [
   { className: "projects", title: "프로젝트" },
 ];
 
-for (let i = 0; i < portfolioSection.length; i++) {
-  const eachSection = portfolioSection[i];
+{
+  /* 
+<div class="my-card-header">
+  <img
+    src="../public/images/img-profile01.png"
+    alt="profile_img"
+    class="profile_img"
+  />
+  <div class="my-card-intro">
+    <p class="card-name">김철수</p>
+    <p class="card-email">test@test.com</p>
+  </div>
+</div>
 
-  const newSection = document.createElement("div");
-  newSection.className = "section";
-  newSection.classList.add(eachSection.className);
-
-  const header = document.createElement("div");
-  header.className = "title-bar";
-
-  const title = document.createElement("h3");
-  title.innerText = eachSection.title;
-
-  const addNewItemBtn = document.createElement("button");
-  addNewItemBtn.innerText = "+ 추가";
-
-  header.appendChild(title);
-  header.appendChild(addNewItemBtn);
-  newSection.appendChild(header);
-  portfolio.appendChild(newSection);
+<div class="my-card-content">
+  안녕하세요! 프론트엔드 개발자를 지망하는 김철수라고 합니다!
+  <button class="modified-button">수정</button>
+</div>
+*/
 }
-
-const modifiedButton = querySelector("button");
