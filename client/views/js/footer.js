7ed1619a113d15ebc170091a39101f23b3405864
구현 <bomji68@naver.com>
2024-05-09 15:07:58 +0900

    signin 오류 메세지 위치 css 추가, footer.js 수정

--- client/views/js/footer.js
+++ client/views/js/footer.js
@@ -3,12 +3,11 @@ window.addEventListener('DOMContentLoaded', function() {
     const footer = document.createElement("div");
     footer.className = "footer";
 
-    footer.innerHTML = `<div class="footer">
+    footer.innerHTML = `
     <div class="footer-logo">
       <img src="../public/images/logo4.png" />
     </div>
     <div class="copywright">@2024 CRAFTERS CORP. All Rights Reserved</div>
-  </div>
   `;
   body.insertAdjacentElement('beforeend', footer);
 });
